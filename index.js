f2fbed6225279adfba8e7d4c9f6d062a8868559c
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2025-08-15 14:55:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connection = require('./config/db');
 const userRoutes = require('./routes/user.routes');
+const productRoutes = require('./routes/product.routes');
+
 
 
 const app = express();
@@ -12,6 +14,7 @@ connection();
 
 // Routes
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
