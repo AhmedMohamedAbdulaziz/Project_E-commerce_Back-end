9802f50f75e466528bffe64455728f91a47db76a
Ahmed Wagih <ahmed.mohamed051@eng-st.cu.edu.eg>
2025-08-16 13:50:53 +0300

    Resolved: category.routes small compilation error

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const categoryController = require('../controllers/category.controller');
-router.route('/')
-    .get(allowTo('admin'),categoryController.getCategories)
-    .post(allowTo('admin'),categoryController.createCateg);
-router.route('/:id')
-    .get(allowTo('admin'),categoryController.getCategoryById)
-    .put(allowTo('admin'),categoryController.updateCategory)
-    .delete(allowTo('admin'),categoryController.deleteCategory);
+const categoryController = require("../controllers/category.controller");
+const allowTo = require("../middlewares/allowedTo");
+
+router
+  .route("/")
+  .get(allowTo("admin"), categoryController.getCategories)
+  .post(allowTo("admin"), categoryController.createCategory);
+router
+  .route("/:id")
+  .get(allowTo("admin"), categoryController.getCategoryById)
+  .put(allowTo("admin"), categoryController.updateCategory)
+  .delete(allowTo("admin"), categoryController.deleteCategory);
 module.exports = router;
 // app.use('/api/categories', categoryRoutes);
