ee8a14d4973b0100def001e0b7608502d10ab2ff
Ahmed Mohamed Abdulaziz <ahmedmohamedabdulaziz2023@gmail.com>
2025-08-28 05:01:55 +0300

    commit it

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connection = require("./config/db");
+const userRoutes = require("./routes/user.routes");
+const productRoutes = require("./routes/product.routes");
+const categoryRoutes = require("./routes/category.routes");
+const orderRoutes = require("./routes/order.routes");
+const cors = require("cors");
+
+const app = express();
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB
+connection();
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
+});
